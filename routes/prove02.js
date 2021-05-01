86e2ecfa3d165bb6620377db01bf206f093a5081
mgweaver <masongweaver@gmail.com>
2021-05-01 16:15:52 -0600

    finish prove 02

--- routes/prove02.js
+++ routes/prove02.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const books = [];
+let books = []
 
-router.get('/',(req, res, next) => {
- 
+router.get('/', (req, res, next) => {
+  res.render('pages/prove02', {
+    title: 'Prove02',
+    path: '/prove02'
+  })
 });
 
-router.post('/add-book',(req, res, next) => {
- 
+router.post('/add-book', (req, res, next) => {
+  books.push({
+    title: req.body.title,
+    summary: req.body.summary
+  })
+  res.render('pages/prove02list', {
+    title: 'Prove02 List',
+    path: '/prove02',
+    books: books,
+  })
 });
 
 module.exports = router;
