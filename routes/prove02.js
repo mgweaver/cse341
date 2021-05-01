71585917f033c5816d492604144cd2553b62a12a
mgweaver <masongweaver@gmail.com>
2021-05-01 14:03:06 -0600

    start on prove 02

---
+++ routes/prove02.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const books = [];
+
+router.get('/',(req, res, next) => {
+ 
+});
+
+router.post('/add-book',(req, res, next) => {
+ 
+});
+
+module.exports = router;
